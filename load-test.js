2b5a2bb405fca4e25c75eefcf3b3aeebf5e1678a
Claudiasuarez28 <cnataliasuarez28@gmail.com>
2026-05-21 19:12:37 -0500

    Prueba de desempeño

---
+++ load-test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 20,
+  duration: '30s',
+};
+
+export default function () {
+  let res = http.get('http://app-load-balancer-1643519133.us-east-1.elb.amazonaws.com/health');
+
+  check(res, {
+    'status es 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
